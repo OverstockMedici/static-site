45091dbce5f828915a4361d5a73b3edbdc987b18
Steve Anderson <steve.anderson@mediciventures.com>
2020-02-26 15:14:45 -0700

    Removed unneeded cors mode since it is there by default

--- src/pages/contact/contact.post.js
+++ src/pages/contact/contact.post.js
@@ -1,7 +1,6 @@
 export default async function postData(url, data) {
     return fetch(url, {
         method: 'POST',
-        mode: 'cors',
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
