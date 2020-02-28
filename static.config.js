507b85f6b7b929b18068aa5bec7528034c8231f1
Steve Anderson <steve.anderson@mediciventures.com>
2020-02-28 10:00:31 -0700

    Removed file system plugin

--- static.config.js
+++ static.config.js
@@ -138,12 +138,12 @@ export default {
         );
     },
     plugins: [
-        [
-            require.resolve('react-static-plugin-source-filesystem'),
-            {
-                location: path.resolve('./src/pages'),
-            },
-        ],
+        // [
+        //     require.resolve('react-static-plugin-source-filesystem'),
+        //     {
+        //         location: path.resolve('./src/pages'),
+        //     },
+        // ],
         require.resolve('react-static-plugin-reach-router'),
         require.resolve('react-static-plugin-sitemap'),
     ],
