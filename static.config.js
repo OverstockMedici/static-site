aa2b9d67ccedac3ba36e781dbd8d03b8a864f8b7
Steve Anderson <steve.anderson@mediciventures.com>
2020-03-02 16:08:44 -0700

    Rolling back to version 7 of react static

--- static.config.js
+++ static.config.js
@@ -138,12 +138,6 @@ export default {
         );
     },
     plugins: [
-        // [
-        //     require.resolve('react-static-plugin-source-filesystem'),
-        //     {
-        //         location: path.resolve('./src/pages'),
-        //     },
-        // ],
         require.resolve('react-static-plugin-reach-router'),
         require.resolve('react-static-plugin-sitemap'),
     ],
