d86834e962dbc4ff124a7493722078ad40bd3589
Vince Howard <vincenguyenhoward@gmail.com>
2020-03-10 14:39:09 -0600

    fixed typos, color choices for on hover for a company in companies page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'components/Router'
+import { Link } from 'components/Router';
 import { MainFooter, FooterLogo, FooterNav, Copyright } from './footer.style';
 import { nav } from '../../content/nav.content';
 
@@ -7,7 +7,9 @@ const reversedLogo = '/img/medici-logo-reversed.png';
 
 export default function Footer() {
     const buildFooterLinks = nav.map(item => (
-        <a href={item.link} key={item.link}>{item.name}</a>
+        <a href={item.link} key={item.link}>
+            {item.name}
+        </a>
     ));
     /* eslint-disable jsx-a11y/anchor-is-valid */
     return (
@@ -15,12 +17,8 @@ export default function Footer() {
             <Link to="/" className="footer-logo-link">
                 <FooterLogo src={reversedLogo} alt="Medici Ventures Logo" />
             </Link>
-            <FooterNav>
-                {buildFooterLinks}
-            </FooterNav>
-            <Copyright>
-                Copyright &copy; MediciVentures Inc. 2020
-            </Copyright>
+            <FooterNav>{buildFooterLinks}</FooterNav>
+            <Copyright>Copyright &copy; Medici Ventures Inc. 2020</Copyright>
         </MainFooter>
     );
 }
--- src/content/companies.content.js
+++ src/content/companies.content.js
@@ -1,7 +1,8 @@
 export const meta = {
     title: 'Companies — Medici Ventures INC',
-    description: 'Medici Ventures companies: Harnessing the World-Changing Power of Blockchain',
-    image: '/img/companies-coliseum-entrance-600.jpg'
+    description:
+        'Medici Ventures companies: Harnessing the World-Changing Power of Blockchain',
+    image: '/img/companies-coliseum-entrance-600.jpg',
 };
 
 export const content = {
@@ -10,139 +11,156 @@ export const content = {
     companies: [
         {
             name: 'tZERO',
-            description: 'Creative minds at tZERO are working on new technology that allows the trade of security tokens in an easy, compliant, and user-friendly manner.',
+            description:
+                'Creative minds at tZERO are working on new technology that allows the trade of security tokens in an easy, compliant, and user-friendly manner.',
             link: 'https://www.tzero.com/',
             linkText: 'tZERO.com',
             image: 'img/tzero-comp-logo.svg',
-            color: '#00ce7d'
+            color: '#00ce7d',
         },
         {
             name: 'Bitt',
-            description: 'Bitt makes online money transfers simple with payment systems that promote social inclusion, financial empowerment and economic growth for this and future generations.',
+            description:
+                'Bitt makes online money transfers simple with payment systems that promote social inclusion, financial empowerment and economic growth for this and future generations.',
             link: 'https://www.bitt.com/',
             linkText: 'bitt.com',
             image: 'img/bitt-comp-logo.svg',
-            color: '#0056c1'
+            color: '#0056c1',
         },
         {
             name: 'Medici Land Governance',
-            description: 'Medici Land Governance is using blockchain and other technologies to bring property ownership information to light and connect it to the global economy.',
+            description:
+                'Medici Land Governance is using blockchain and other technologies to bring property ownership information to light and connect it to the global economy.',
             link: 'http://www.mediciland.com/',
             linkText: 'MediciLand.com',
             image: 'img/mlg-comp-logo.svg',
-            color: '#5dc55f'
+            color: '#5dc55f',
         },
         {
             name: 'GrainChain',
-            description: 'GrainChain\'s innovative platform can facilitate prompt payment to suppliers and farmers, and the immediate availability of tradable commodities to buyers.',
+            description:
+                "GrainChain's innovative platform can facilitate prompt payment to suppliers and farmers, and the immediate availability of tradable commodities to buyers.",
             link: 'https://www.grainchain.io/',
             linkText: 'GrainChain.io',
             image: 'img/grain-chain-comp-logo.png',
-            color: '#3db54b'
+            color: '#3db54b',
         },
         {
             name: 'Voatz',
-            description: 'The minds behind Voatz intend to redefine how the world votes by replacing long lines and outdated voting systems with everyday smart devices with biometric ID and blockchain irrefutability to provide accurate and tamper free voting.',
+            description:
+                'The minds behind Voatz intend to redefine how the world votes by replacing long lines and outdated voting systems with everyday smart devices with biometric ID and blockchain irrefutability to provide accurate and tamper free voting.',
             link: 'https://voatz.com/',
             linkText: 'Voatz.com',
             image: 'img/voatz-comp-logo.png',
-            color: '#0065a8'
+            color: '#0065a8',
         },
         {
             name: 'Evernym',
-            description: 'Evernym has a revolutionary new digital identity solution that will reduce costs, transform customer experiences, and outperform competitors.',
+            description:
+                'Evernym has a revolutionary new digital identity solution that will reduce costs, transform customer experiences, and outperform competitors.',
             link: 'https://www.evernym.com/',
             linkText: 'Evernym.com',
             image: 'img/evernym-logo-small.png',
-            color: '#86c044'
+            color: '#86c044',
         },
         {
             name: 'SettleMint',
-            description: 'The enthusiastic team at SettleMint has created a platform which will help you integrate blockchain technologies into your business by providing solutions for nearly every business use-case.',
+            description:
+                'The enthusiastic team at SettleMint has created a platform which will help you integrate blockchain technologies into your business by providing solutions for nearly every business use-case.',
             link: 'https://settlemint.com/',
             linkText: 'SettleMint.com',
             image: 'img/settlemint-comp-logo.svg',
-            color: '#31bcb5'
+            color: '#31bcb5',
         },
         {
             name: 'Ripio',
-            description: 'Ripio has created a digital wallet that provides a simple way to send and receive money, buy and sell bitcoins, and even pay in installments without credit card on any website accepting Bitcoin.',
+            description:
+                'Ripio has created a digital wallet that provides a simple way to send and receive money, buy and sell bitcoins, and even pay in installments without credit card on any website accepting Bitcoin.',
             link: 'https://www.ripio.com/en/',
             linkText: 'ripio.com',
             image: 'img/ripio-comp-logo.png',
-            color: '#0066b9'
+            color: '#0066b9',
         },
         {
             name: 'Spera',
-            description: 'Spera’s all-in-one freelance website platform helps independent business owners by providing tools that will help find and post jobs, manage projects, and get paid with a free Spera merchant account.',
+            description:
+                'Spera’s all-in-one freelance website platform helps independent business owners by providing tools that will help find and post jobs, manage projects, and get paid with a free Spera merchant account.',
             link: 'https://spera.io/',
             linkText: 'spera.io',
             image: 'img/spera-comp-logo.png',
-            color: '#00486a'
-        },       
+            color: '#00486a',
+        },
         {
             name: 'PeerNova',
-            description: 'Founded in 2013 by experts in blockchain, big data, and financial infrastructure, PeerNova’s platform simplifies processes and guarantees data integrity across systems.',
+            description:
+                'Founded in 2013 by experts in blockchain, big data, and financial infrastructure, PeerNova’s platform simplifies processes and guarantees data integrity across systems.',
             link: 'http://peernova.com/',
             linkText: 'PEERNOVA.com',
             image: 'img/peernova-comp-logo.png',
-            color: '#552eff'
+            color: '#475C66',
         },
         {
             name: 'Finclusive',
-            description: 'FinClusive provides a high-compliance, blockchain-architected digital banking, routing, and payments platform that is accessible to companies and individuals underserved, excluded, and derisked by traditional banking.',
+            description:
+                'FinClusive provides a high-compliance, blockchain-architected digital banking, routing, and payments platform that is accessible to companies and individuals underserved, excluded, and derisked by traditional banking.',
             link: 'https://www.finclusivecapital.com/',
             linkText: 'FinClusiveCapitol.com',
             image: 'img/finclusive-comp-logo.svg',
-            color: '#ff861d'
+            color: '#ff861d',
         },
         {
             name: 'Netki',
-            description: 'Netki is building the future of trust with advanced biometrics, a fully automated, scalable solution, and deep industry knowledge, to ensure compliance in all your digital transactions.',
+            description:
+                'Netki is building the future of trust with advanced biometrics, a fully automated, scalable solution, and deep industry knowledge, to ensure compliance in all your digital transactions.',
             link: 'https://netki.com/',
             linkText: 'NETKI.com',
             image: 'img/netki-comp-logo.svg',
-            color: '#57278f'
+            color: '#57278f',
         },
         {
             name: 'Minds',
-            description: 'Minds is building a new model for content creators to take back their Internet freedom, revenue, and social reach. Creators get paid in crypto for their contributions to the community.',
+            description:
+                'Minds is building a new model for content creators to take back their Internet freedom, revenue, and social reach. Creators get paid in crypto for their contributions to the community.',
             link: 'https://www.minds.com/',
             linkText: 'MINDS.com',
             image: 'img/minds-comp-logo.svg',
-            color: '#e1ac00'
+            color: '#e1ac00',
         },
         {
             name: 'Vinsent',
-            description: 'Vinsent is a blockchain based platform that enables thousands of wineries to streamline funding by selling wine futures and developing direct connections with their consumers.',
+            description:
+                'Vinsent is a blockchain based platform that enables thousands of wineries to streamline funding by selling wine futures and developing direct connections with their consumers.',
             link: 'https://www.vinsent.wine/',
             linkText: 'Vinsent.com',
             image: 'img/vinsent-comp-logo.svg',
-            color: '#e74d59'
+            color: '#e74d59',
         },
         {
             name: 'Symbiont',
-            description: 'Symbiont Smart Securities® technology provides institutions with a unified ledger for sharing business logic and market data through immutable blockchain technology.',
+            description:
+                'Symbiont Smart Securities® technology provides institutions with a unified ledger for sharing business logic and market data through immutable blockchain technology.',
             link: 'https://symbiont.io/',
             linkText: 'symbiont.io',
             image: 'img/symbiont-comp-logo.png',
-            color: '#ff6000'
+            color: '#ff6000',
         },
         {
             name: 'Factom',
-            description: 'Factom has created a breakthrough authentication solution that lets you secure physical documents on the Blockchain helping create, organize, and report documents proofs, data, decisions and activities within your organization.',
+            description:
+                'Factom has created a breakthrough authentication solution that lets you secure physical documents on the Blockchain helping create, organize, and report documents proofs, data, decisions and activities within your organization.',
             link: 'https://www.factom.com/',
             linkText: 'Factom.com',
             image: 'img/factom-comp-logo.svg',
-            color: '#f1ba13'
+            color: '#f1ba13',
         },
         {
             name: 'Votem',
-            description: 'Votem\'s goal is to make mobile voting a reality. In 2015, they held a world-wide challenge for experts to create a hack-proof voting system. Out of 110 submissions from 32 countries, the winning solution was a blockchain system.',
+            description:
+                "Votem's goal is to make mobile voting a reality. In 2015, they held a world-wide challenge for experts to create a hack-proof voting system. Out of 110 submissions from 32 countries, the winning solution was a blockchain system.",
             link: 'https://votem.com/',
             linkText: 'Votem.com',
             image: 'img/votem-comp-logo.png',
-            color: '#032a4a'
-        }
-    ]
+            color: '#032a4a',
+        },
+    ],
 };
