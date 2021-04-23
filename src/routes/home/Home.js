dd929aba32ea1971b31822e955425bc51ae1f1a0
Erik Lindblom <erik@baens.net>
2021-04-23 13:20:03 -0600

    Update home to be companies page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,150 +1,36 @@
 import React, { useEffect, useState, useRef, useContext } from 'react';
 import smoothscroll from 'smoothscroll-polyfill';
 import Meta from '../../components/Meta';
-import { Main, Hero, DownButton, Overview, Industries, WhoIs, LearnMore } from './home.style';
+import { Main, Hero, ContactUs } from './home.style';
 import { ButtonLink } from '../../components/ButtonLink';
-import { home, meta } from '../../content/home.content';
+import { content, meta } from '../../content/companies.content';
 import { Context } from '../../store';
 import OverviewComponent from './OverviewComponent';
+import MobileCompanyList from '../companies/MobileCompanyList';
+import DesktopCompanyList from '../companies/DesktopCompanyList';
+const { heading1, heading1b } = content;
 
-const advancingBlockchainTechnology = 'img/ABT.svg';
-const peaceColiseum = 'img/peace-coliseum.png';
-const peaceColiseumTall = 'img/peace-coliseum-350-tall.jpg';
-const peaceColiseum380 = 'img/peace-coliseum-380.jpg';
-const peaceColiseum600 = 'img/peace-coliseum-600.jpg';
-const peaceColiseum870 = 'img/peace-coliseum-870.jpg';
-const peaceColiseumFull = 'img/peace-coliseum-589-full.jpg';
-const courtyard = 'img/peace-coliseum-courtyard.png';
-const courtyardTall = 'img/peace-coliseum-courtyard-350-tall.jpg';
-const courtyard380 = 'img/peace-coliseum-courtyard-380.jpg';
-const courtyard600 = 'img/peace-coliseum-courtyard-600.jpg';
-const courtyard870 = 'img/peace-coliseum-courtyard-870.jpg';
-const courtyardFull = 'img/peace-coliseum-courtyard-589-full.jpg';
 
 export default function Home() {
-    const { store, dispatch } = useContext(Context);
-    const [overviewAnimation, setOverviewAnimation] = useState(false);
-    const [industriesAnimation, setIndustriesAnimation] = useState(false);
+    const { store } = useContext(Context);
 
-    const refOverview = useRef(null);
-    const refIndustriesContainer = useRef(null);
-
-    useEffect(() => {
-        smoothscroll.polyfill();
-
-        // Observer to add the animated class to initial animations
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.intersectionRatio > 0.2 && entry.target === refOverview.current) {
-                    setOverviewAnimation(true);
-                } else if (entry.intersectionRatio > 0.2 && entry.target === refIndustriesContainer.current) {
-                    setIndustriesAnimation(true);
-                }
-            });
-        }, {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.21
-        });
-
-        observer.observe(refOverview.current);
-        observer.observe(refIndustriesContainer.current);
-
-        return () => {
-            observer.disconnect();
-        };
-    }, []);
-
-    const scrollDown = () => {
-        const height = window.innerHeight * 0.9;
-
-        window.scroll({
-            top: height,
-            behavior: 'smooth'
-        });
-    };
-
-    const buildOverview = () => (
-        home.overview.map(({heading, content}, index) => (
-            <OverviewComponent 
-                key={heading}
-                styleName={`overview-${index + 1}`}
-                {...{ heading, content }}
-            />
-        ))
-    );
-
-    const buildIndustries = () => ( // TODO: remove text from services .png files
-        home.industries.map(industry => (
-            <img src={industry.imgUrl} alt={industry.caption} key={industry.caption} />
-        ))
-    );
-
-    const buildWhoIsParagraphs = paragraphs => (
-        paragraphs.map(paragraph => (
-            <p key={paragraph}>{paragraph}</p>
-        ))
+    const renderCompanyList = () => (
+        store.isMobile ?
+            <MobileCompanyList /> :
+            <DesktopCompanyList />
     );
 
     return (
-        <div className="home">
-            <Meta {...meta} />
+        <div className="companies">
             <Main role="main">
+                <Meta {...meta} />
                 <Hero>
-                    <img src={advancingBlockchainTechnology} alt="Advancing Blockchain Technology" />
-                    <DownButton onClick={scrollDown} aria-label="Arrow Down" title="Arrow Down" />
+                    <h1><span>{heading1}</span><br />{heading1b}</h1>
                 </Hero>
-
-                <Overview
-                    ref={refOverview}
-                    id="overview"
-                    className={overviewAnimation && 'animated'}
-                >
-                    {buildOverview()}
-                </Overview>
-
-                <Industries className="industries">
-                    <h2>Medici Ventures focuses on:</h2>
-                    <div
-                        ref={refIndustriesContainer}
-                        className={`industries-container ${industriesAnimation && 'animated'}`}
-                    >
-                        {buildIndustries()}
-                    </div>
-                </Industries>
-
-                <WhoIs>
-                    <div className="who-is-1">
-                        <picture>
-                            <source media="(min-width: 1070px)" srcSet={peaceColiseumFull} />
-                            <source media="(min-width: 870px)" srcSet={peaceColiseumTall} />
-                            <source media="(min-width: 601px)" srcSet={peaceColiseum870} />
-                            <source media="(min-width: 400px)" srcSet={peaceColiseum600} />
-                            <source media="(max-width: 399px)" srcSet={peaceColiseum380} />
-                            <img src={peaceColiseum} alt="Medici Ventures Headquarters" />
-                        </picture>
-                        <div>
-                            <h2>Who is Medici Ventures</h2>
-                            {buildWhoIsParagraphs(home.whoIs1)}
-                        </div>
-                    </div>
-                    <div className="who-is-2">
-                        <picture>
-                            <source media="(min-width: 1070px)" srcSet={courtyardFull} />
-                            <source media="(min-width: 870px)" srcSet={courtyardTall} />
-                            <source media="(min-width: 601px)" srcSet={courtyard870} />
-                            <source media="(min-width: 400px)" srcSet={courtyard600} />
-                            <source media="(max-width: 399px)" srcSet={courtyard380} />
-                            <img src={courtyard} alt="Medici Ventures Headquarters Courtyard" />
-                        </picture>
-                        <div>
-                            {buildWhoIsParagraphs(home.whoIs2)}
-                        </div>
-                    </div>
-                </WhoIs>
-                <LearnMore>
-                    <ButtonLink to="/companies" isMobile={store.isMobile}>Learn More</ButtonLink>
-                </LearnMore>
+                {renderCompanyList()}
+                <ContactUs>
+                    <ButtonLink to="/contact" isMobile={store.isMobile}>Contact Us</ButtonLink>
+                </ContactUs>
             </Main>
         </div>
     );
