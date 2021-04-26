e560b5225899121090eaeedb22ae7f6f43a5cde8
Erik Lindblom <erik@baens.net>
2021-04-25 22:07:02 -0600

    Update footer list and copywrite

--- src/content/nav.content.js
+++ src/content/nav.content.js
@@ -1,15 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export const nav = [
-    {
-        name: 'Companies',
-        link: '/companies'
-    },
-    {
-        name: 'News',
-        link: '/news'
-    },
-    {
-        name: 'Contact',
-        link: '/contact'
-    }
 ];
